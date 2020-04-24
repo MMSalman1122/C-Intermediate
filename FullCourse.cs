41cf4807635052bb252c786051cbfcf567761a5a
MMSalman1122 <43203395+MMSalman1122@users.noreply.github.com>
2020-04-24 18:37:13 +0500

    Today's riefing:

--- FullCourse.cs
+++ FullCourse.cs
@@ -1,33 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
 
-namespace test
-{
-    public class Person
-    {
-        private DateTime _birthdate;
-
-        public void SetBirthdate(DateTime birthdate)
-        {
-            _birthdate = birthdate;
-        }
-        public DateTime GetBirthdate()
-        {
-            return _birthdate;
-        }
-    }
-    
-    class Program
-    {
-        public static void Main()
-        {
-            var person = new Person();
-            person.SetBirthdate(new DateTime(1982,1,1));
-            Console.WriteLine(person.GetBirthdate());
-            Console.ReadLine();
-        }
-     
-    }
-}
+Briefing:
+Section 2 
+Lecture 8-12 complete
+Time of course today: 69 min
+Course status:12/45 lec 0.26% complete
