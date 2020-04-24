3bd11dfd6b976c5ae2436911a1b7e7f385946f72
MMSalman1122 <43203395+MMSalman1122@users.noreply.github.com>
2020-04-24 17:30:34 +0500

    Section 2 Lecture 1(Fields 9 min)

--- FullCourse.cs
+++ FullCourse.cs
@@ -5,79 +5,34 @@ using System.Text;
 
 namespace test
 {
-    public class Point
+    public class Customer
     {
-        public int x;
-        public int y;
-        public Point(int x,int y)
+        public int Id;
+        public string Name;
+        public readonly List<Oreder> Orders= new List<Oreder>();// initialize empty list once only
+
+        
+        public Customer(int id)
         {
-            this.x = x;
-            this.y = y;
+            this.Id = Id;
         }
-        public void Move(int x, int y)
+        public Customer(int id, string name)
+            :this(id)
         {
-            this.x = x;
-            this.y = y;
-        }
-        public void Move(Point newLocation)
-           
-        {   if(newLocation==null)
-            {
-                throw new ArgumentNullException("newLocation");
-            }
-            Move(newLocation.x, newLocation.y);
+            this.Name = name;
         }
     }
-    public class Calculator
+    public class Oreder
     {
-        public int add(params int[] numbers)
-        {
-            var sum = 0;
-            foreach(var number in numbers)
-            {
-                sum += number;
-            }
-            return sum;
-        }
+
     }
     class Program
     {
         public static void Main()
         {
-            int number;
-            var result=int.TryParse("abc",out number);
-            if (result)
-            { Console.WriteLine(); }
-            else
-            {
-                Console.WriteLine("Conversion is not possible");
-            }
+            
             Console.ReadLine();
         }
-
-        static void UseParams()
-        {
-            var calculator = new Calculator();
-            Console.WriteLine(calculator.add(1, 2, 3, 4, 5));
-            Console.WriteLine(calculator.add(new int[] { 1, 2, 3, 4, 5 }));
-        }
-        static void UsePoint()
-        {
-            try
-            {
-                var point = new Point(10, 20);
-                point.Move(null);
-                Console.WriteLine("Point is at ({0},{1})", point.x, point.y);
-                point.Move(100, 200);
-                Console.WriteLine("Point is at ({0},{1})", point.x, point.y);
-            }
-            catch (Exception)
-            {
-
-                Console.WriteLine("Error occure");
-            }
-        }
-
      
     }
 }
