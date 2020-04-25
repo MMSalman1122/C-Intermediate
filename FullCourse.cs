4f2a3be08bf2d8a6efef927a754b54213611e2a5
MMSalman1122 <43203395+MMSalman1122@users.noreply.github.com>
2020-04-25 20:58:42 +0500

    Section 3 Lecture 18(Inheritnece 8 min)

--- FullCourse.cs
+++ FullCourse.cs
@@ -1,1 +1,45 @@
-Section 3 Lecture 17 (Class coupling)
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace test
+{
+   public class PresentationObject
+    {
+        public int width { get; set; }
+        public int height { get; set; }
+
+        public void Copy()
+        {
+            Console.WriteLine("Object is copied to clipbord");
+        }
+        public void Duplicate()
+        {
+            Console.WriteLine("Object was duplicated");
+        }
+    }
+    public class Text: PresentationObject
+    {
+        public int FontSize { get; set; }
+        public string FontName { get; set; }
+
+        public void AddHyperlink(string url)
+        {
+            Console.WriteLine("We added a link to "+ url);
+        }
+    }
+  
+    class Program
+    {
+        public static void Main()
+        {
+            var text = new Text();
+            text.width = 100;
+            text.Copy();
+          
+            Console.ReadLine();
+        }
+     
+    }
+}
