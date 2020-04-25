b904c825d8933e75208a1fed1090c20c730613db
MMSalman1122 <43203395+MMSalman1122@users.noreply.github.com>
2020-04-25 17:01:34 +0500

    Section 2 Lec 14(Indexers 10 min)

--- FullCourse.cs
+++ FullCourse.cs
@@ -5,21 +5,14 @@ using System.Text;
 
 namespace test
 {
-    public class Person
+   public class HttpCookie
     {
-        public Person(DateTime birthdate)
+        private readonly Dictionary<string, string> _dictionary= new Dictionary<string, string>();
+     
+        public string this[string key]
         {
-            Birthdate = birthdate;
-        }
-        public DateTime Birthdate { get;private set; }
-        public int Age
-        {
-            get
-            {
-                var timeSpan=DateTime.Today - Birthdate;
-                var years = timeSpan.Days / 365;
-                return years;
-            }
+            get { return _dictionary[key]; }
+            set { _dictionary[key] = value; }
         }
     }
     
@@ -27,8 +20,10 @@ namespace test
     {
         public static void Main()
         {
-            var person = new Person(new DateTime(1997, 12, 1));
-            Console.WriteLine(person.Age);
+            var cookie = new HttpCookie();
+            cookie["name"] = "salman";
+            Console.WriteLine(cookie["name"]);
+          
             Console.ReadLine();
         }
      
