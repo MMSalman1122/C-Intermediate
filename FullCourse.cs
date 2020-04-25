b820a90885d14d59771f77586ba886d999d7f2e0
MMSalman1122 <43203395+MMSalman1122@users.noreply.github.com>
2020-04-25 16:20:23 +0500

    Section 2 Lec 13(Properties(16min))

--- FullCourse.cs
+++ FullCourse.cs
@@ -1,6 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
-Briefing:
-Section 2 
-Lecture 8-12 complete
-Time of course today: 69 min
-Course status:12/45 lec 0.26% complete
+namespace test
+{
+    public class Person
+    {
+        public Person(DateTime birthdate)
+        {
+            Birthdate = birthdate;
+        }
+        public DateTime Birthdate { get;private set; }
+        public int Age
+        {
+            get
+            {
+                var timeSpan=DateTime.Today - Birthdate;
+                var years = timeSpan.Days / 365;
+                return years;
+            }
+        }
+    }
+    
+    class Program
+    {
+        public static void Main()
+        {
+            var person = new Person(new DateTime(1997, 12, 1));
+            Console.WriteLine(person.Age);
+            Console.ReadLine();
+        }
+     
+    }
+}
