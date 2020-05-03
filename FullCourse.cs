23477bcfce9581eb38e946d108d860981d0ec8e3
MMSalman1122 <43203395+MMSalman1122@users.noreply.github.com>
2020-05-03 05:29:20 +0500

    Section 4 Lcture 25(Boxing Unboxing)

--- FullCourse.cs
+++ FullCourse.cs
@@ -6,49 +6,17 @@ using System.Text;
 
 namespace test
 {
-    public class Shape
-    {
-        public int Height { get; set; }
-        public int Width { get; set; }
-        public int x { get; set; }
-        public int y { get; set; }
-
-        public void Draw()
-        {
-
-        }
-    }
-    public class Text: Shape
-    {
-        public int FontSize { get; set; }
-        public int FontName { get; set; }
-
-    }
+    
     class Program
     {
 
         public static void Main()
         {
-            Text text = new Text();
-            Shape shape = text;
-
-
-            text.Width = 200;
-            shape.Width = 100;// overwrites the value of text.wdth
-            Console.WriteLine(text.Width);
-
-            // StreamReader reader = new StreamReader(new MemoryStream());
-            ArrayList list = new ArrayList();
+            var list = new ArrayList();
             list.Add(1);
-            list.Add("lol");
-            list.Add(new Text());
-            // down cast
-            Shape shap = new Text();
-            // in shap. attributes of text are available
+            list.Add(DateTime.Today);
 
-            Text textt = (Text)shap;
-            // in textt. every property is available
-
+            
             Console.ReadLine();
         }
 
