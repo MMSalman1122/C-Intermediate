a367200e72da2231861513c311b4bd55ac4fb3b3
MMSalman1122 <43203395+MMSalman1122@users.noreply.github.com>
2020-05-11 16:46:28 +0500

    Section 2 Lecture 8 (Lambda Eexpression)

--- Advance_Course.cs
+++ Advance_Course.cs
@@ -6,31 +6,40 @@ using System.Text;
 
 namespace test
 {
+    public class BookRepository
+    {
+        public List<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book() {Title="Title 1",Price=5 },
+                new Book() {Title="Title 2",Price=7 },
+                new Book() {Title="Title 3",Price=17 },
+            };
+        }
+    }
+    public class Book
+    {
+        public string Title { get; set; }
+        public int Price { get; set; }
+    }
     
         
     class Program
     {
         static void Main(string[] args)
         {
-
-            //number => number * number
-            //()=>...
-            //x=>...
-            //(x,y,z)=>...
-
-            Func<int, int> squre =number=>number*number;
-
-            Console.WriteLine(squre(2));
-
-
-            const int factor = 5;
-
-
-
-            Func<int, int> multiplier = n => n * factor;
-            Console.WriteLine(multiplier(2));
-
+            var books = new BookRepository().GetBooks();
+           var cheapBooks= books.FindAll(IsCheaper);
+            foreach (var book in cheapBooks)
+            {
+                Console.WriteLine(book.Title);
+            }
             Console.ReadLine();
+        }
+        static bool IsCheaper(Book book)
+        {
+            return book.Price < 10;
         }
        
     }
