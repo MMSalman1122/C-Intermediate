ab7258399bb2e7d73ff2a4f1134ff737d5cc81b5
MMSalman1122 <43203395+MMSalman1122@users.noreply.github.com>
2020-05-11 16:20:06 +0500

    section 2 Lecture 8 (Lambda Expressions)

--- Advance_Course.cs
+++ Advance_Course.cs
@@ -6,69 +6,33 @@ using System.Text;
 
 namespace test
 {
-    public class Photo
-    {
-        public static Photo Load(String path)
-        {
-            return new Photo();
-        }
-        public void Save()
-        {
-
-        }
-    }
-    public class PhotoFilters
-    {
-        public void ApplyBrightness(Photo photo)
-        {
-            Console.WriteLine("Apply Brightness");
-        }
-        public void ApplyContrast(Photo photo)
-        {
-            Console.WriteLine("Apply Contrast");
-        }
-        public void Resize(Photo photo)
-        {
-            Console.WriteLine("Resize photo");
-        }
-    }
-    public class PhotoProcessor
-    {
-        public delegate void PhotoFilterHandler(Photo photo);
-        public void Process(string path, PhotoFilterHandler filterHandler)
-        {
-            var photo = Photo.Load(path);
-            filterHandler(photo);
-            
-            photo.Save();
-            
-            
-        }
-    }
+    
         
     class Program
     {
         static void Main(string[] args)
         {
-            var processor = new PhotoProcessor();
-            var filter = new PhotoFilters();
-            PhotoProcessor.PhotoFilterHandler filterHandler = filter.ApplyBrightness;
 
-            filterHandler += filter.ApplyContrast;
-            //add to deligate
-            filterHandler += RemoveRedEyerFilter;
+            //number => number * number
+            //()=>...
+            //x=>...
+            //(x,y,z)=>...
 
-            processor.Process("dhajhdjska", filterHandler);
-            
-           
+            Func<int, int> squre =number=>number*number;
+
+            Console.WriteLine(squre(2));
+
+
+            const int factor = 5;
+
+
+
+            Func<int, int> multiplier = n => n * factor;
+            Console.WriteLine(multiplier(2));
 
             Console.ReadLine();
         }
-        // create own filter
-        static void RemoveRedEyerFilter(Photo photo)
-        {
-            Console.WriteLine("Red eye ");
-        }
+       
     }
 
 
