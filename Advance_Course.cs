6064ee3219ab1ec01df6f5f5da14287e9df69bd3
MMSalman1122 <43203395+MMSalman1122@users.noreply.github.com>
2020-05-11 21:53:43 +0500

    Section 2 Lec 9(Events)

--- Advance_Course.cs
+++ Advance_Course.cs
@@ -13,19 +13,48 @@ namespace test
     }
     public class VideoEncoder
     {
+        //Publish that an event is occuring
+        //1 define deligat
+        //2. define event
+        //3.Rais the event
+
+        public delegate void VideoEncoderEventHandler(object source, EventArgs args);
+        public event VideoEncoderEventHandler VideoEncoded;
+
+
         public void Encode(video video)
         {
             Console.WriteLine("Encoding video");
             Thread.Sleep(1000);
+
+            OnVideoEncoded();
         }
-    }   
+        public virtual void OnVideoEncoded()
+        {
+            if (VideoEncoded != null)
+                VideoEncoded(this, EventArgs.Empty);
+        }
+    } 
+    public class MailService
+    {
+        public void OnvideoEncoded(Object source,EventArgs e)
+        {
+            Console.WriteLine("sending email");
+        }
+    }
             
     class Program
     {
         static void Main(string[] args)
         {
             var video = new video() { Title = "video 1" };
-            var videoEncoder = new VideoEncoder();
+            var videoEncoder = new VideoEncoder();//publisher
+            var mailservice = new MailService();//subscriber
+
+            videoEncoder.VideoEncoded += mailservice.OnvideoEncoded;
+
+
+
             videoEncoder.Encode(video);
             Console.ReadLine();
         }
