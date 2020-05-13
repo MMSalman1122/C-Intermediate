5144fccb217f37515fa48ad8bd047086b0f6af11
MMSalman1122 <43203395+MMSalman1122@users.noreply.github.com>
2020-05-13 19:38:54 +0500

    Section 2 Lec 12(Nullable Types)

--- Advance_Course.cs
+++ Advance_Course.cs
@@ -8,58 +8,21 @@ using System.Threading;
 
 namespace test
 {
-    //Extension methods
-    public class Book
-    {
-        public string Title { get; set; }
-        public float Price { get; set; }
-    }
-
-    public class BookRepository
-    {
-        public IEnumerable<Book>GetBooks()
-        {
-            return new List<Book>
-            {
-                new Book(){Title= "Add.Net step by step", Price=5 },
-                new Book(){Title= "ASP.Net mvc", Price=9.99f },
-                new Book(){Title= "Add.Net WEb", Price=12 },
-                new Book(){Title= "C# Advance", Price=7 },
-                new Book(){Title= "step by step", Price=9 }
-            };
-                
-        }
-    }
-
+    
     class Program
     {
         static void Main(string[] args)
         {
-            var books = new BookRepository().GetBooks();
-            //Linq Query Operators
-            var cheaperBooks = from b in books
-                               where b.Price < 10
-                               orderby b.Title
-                               select b.Title;
-
-            //Linq extension methods
-            var cheapBooks = books
-                                    .Where(b => b.Price < 10)
-                                    .OrderBy(b=> b.Title)
-                                    .Select(b => b.Title);
+            DateTime? date = null;
+            Console.WriteLine("GetValue"+date.GetValueOrDefault());
+            Console.WriteLine("HasValue" + date.HasValue);
+            //Console.WriteLine("Value" + date.Value);
 
 
-            foreach (var book in cheapBooks)
-            {
-                 Console.WriteLine(book);
-            }
+            DateTime? date2= new DateTime(2014, 1, 1);
+            DateTime date3 = date2.GetValueOrDefault();
 
-            //for single return
-            var book1 = books.Single(b => b.Title == "step by step");
-            Console.WriteLine(book1.Price);
-            //for count
-            var count = books.Count();
-            Console.WriteLine(count);
+            Console.WriteLine(date3);
 
             Console.ReadLine();
         }
